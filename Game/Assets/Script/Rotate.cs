8e561addc76c0e613a4bab34a0eb02c18439a5e6
RitaMordio <jun_yen_chen@hotmail.com>
2013-07-12 14:08:33 +0800

    cube & rotate.cs

---
+++ Game/Assets/Script/Rotate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	transform.Rotate(0, 2, 0);
+
+	}
+}
